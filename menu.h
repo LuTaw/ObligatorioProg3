7e99db8703ab0d30beab37bdb0161f2051576483
lmele <lmele@bps.gub.uy>
2025-07-14 08:41:00 -0300

    removidos metodos privados

--- menu.h
+++ menu.h
@@ -9,14 +9,4 @@ void desplegarMenuPrincipal();
 //procesa opcion elegida en el menu princiapl
 void procesarValorMenuPrincipal(int num);
 
-//desplegar menu secundario
-void desplegarSegundoMenu();
-
-//procesa opcion elegida en el menu secundario
-void procesarValorSegundoMenu(int opcionIngresada, Torneo &torneo, Participantes &participantes, Encuentros &encuentros);
-
-//maneja segundo menu
-void segundoMenu(Torneo &torneo, Participantes &participantes);
-
-
 #endif // MENU_H_INCLUDED
